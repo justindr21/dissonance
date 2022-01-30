a472e2e49d4a3efaaad3d0c4e85692a33feabc5e
justindr21 <justin.lie.rahardjo@gmail.com>
2022-01-29 23:13:25 -0800

    add audio

---
+++ Dissonance/Assets/SfxManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxManager : MonoBehaviour
+{
+
+    public AudioSource Audio;
+
+    public AudioClip Click;
+
+    public static SfxManager sfxInstance;
+    
+    private void Awake() {
+        if (sfxInstance != null && sfxInstance != this) {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        sfxInstance = this;
+        DontDestroyOnLoad(this);    
+    }
+
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
