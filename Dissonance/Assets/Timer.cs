5f012bb4737a02765d6bdf6d4f609625183a0768
MargaretPatel <margaretpatel@icloud.com>
2022-01-29 23:10:07 -0500

    Jordan's branch update

---
+++ Dissonance/Assets/Timer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+public float timeValue = 120;
+public Text timeText;
+
+    // Update is called once per frame
+    void Update() {
+        if (timeValue > 0) {
+            timeValue -= Time.deltaTime;
+        } else {
+            timeValue = 0;
+        }
+
+        displayTime(timeValue);
+    }
+
+    void displayTime(float time) {
+        if(time < 0) {
+            time = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(time / 60);
+        float seconds = Mathf.FloorToInt(time % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
