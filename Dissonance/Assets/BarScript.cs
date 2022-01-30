3b586e359ccf1b8653c29f460844f82d121920e4
Jordan Watterston <jorwat@shaw.ca>
2022-01-29 16:09:57 -0800

    editing hitboxes, fixed direction

--- Dissonance/Assets/BarScript.cs
+++ Dissonance/Assets/BarScript.cs
@@ -6,13 +6,15 @@ public class BarScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    
+
 // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D other) {
         if(Input.GetKey(KeyCode.Space)) {
             Vector3 offset = new Vector3(0.25f, 0, 0);
-            transform.position -= offset;
+            transform.position += offset;
+            Destroy(other.gameObject);
             Debug.Log("hit");
+
         }
     }
     // Update is called once per frame
